9588c6ec85b9aba9dca14bb527dc8a69ea7a401d
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-07-21 15:23:13 -0400

    request json

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { Card, Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import DataContext from "../context/Datacontext"
 
 const Cards = () => {
@@ -9,15 +10,17 @@ const Cards = () => {
     <>
       {data.map((item) => (
         <Card
-        key={item.id}
-        className="p-2"
-         style={{ backgroundImage: `url(${item.image})` }}
+          key={item.id}
+          className="p-2"
+          style={{ backgroundImage: `url(${item.image})` }}
         >
           <Card.Body>
-          <div className="container-card">
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>{item.description}</Card.Text>
-            <Button variant="dark">GitHub</Button>
+            <div className="container-card">
+              <Card.Title>{item.name}</Card.Title>
+              <Card.Text>{item.description}</Card.Text>
+              <Link to={item.url} target="_blank">
+                <Button variant="dark">GitHub</Button>
+              </Link>
             </div>
           </Card.Body>
         </Card>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-dark text-center text-light p-2">
-            <h5>@Hector_Gonzalez, 2023.</h5>
+            <h5>Gracias por visitar mi pagina que tengas buena exploracion!🚀 .@Hector_Gonzalez, 2023. </h5>
         </footer>
     );
 }
