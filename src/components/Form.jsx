5d97e1cb6471c8c73e47895e0a8a1be49023c807
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-07-28 17:18:54 -0400

    [fix]-form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,60 @@
-import { Form } from "react-bootstrap"
+import { useState } from "react"
+import { Form, Button} from "react-bootstrap"
+import emailjs from "emailjs-com"
 
 const Contacto = () => {
+  const initialState = {
+    from_name: "",
+    from_email: "",
+    message: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_a0x141h", "template_oi5ampe", e.target, "c4a3zcM5kTNKDmSEk")
+      .then((res) => {
+        console.log(res);
+        alert("Mensaje enviado correctamente.");
+        setFormData(initialState);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Hubo un error en el envío del mensaje.");
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
   return (
-    <Form className="container-form">
+    <Form className="container-form" onSubmit={handleSubmit}>
       <Form.Group className="mb-3 form-group bg-dark">
-        <Form.Label className="container-fluid my-5 fw-bold text-white text-decoration-none text-uppercase">Contacto :</Form.Label>
+        <Form.Label className="container-fluid my-4 fw-bold text-white text-decoration-none text-uppercase">Contacto</Form.Label>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label className="text-white text-decoration-none">Nombre</Form.Label>
+          <Form.Control type="text" name="from_name" value={formData.from_name} onChange={handleChange} required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Label className="text-white text-decoration-none">Dirección de correo electrónico</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" name="from_email" value={formData.from_email} onChange={handleChange} required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label className="text-white text-decoration-none">Escribe tu mensaje aquí:</Form.Label>
+          <Form.Control as="textarea" rows={3} name="message" value={formData.message} onChange={handleChange} required />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+        Enviar
+      </Button>
         <ul className="text-white text-decoration-none">
           <li><span className="fw-bold me-3">Mail:</span> g.p.hector.alejandro@gmail.com</li>
           <li><span className="fw-bold me-2">Fono:</span> +569-50882834</li>
