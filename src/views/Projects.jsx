b857f1be35956dbf812dba9146c77094225a20ac
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-11-10 11:59:23 -0300

    [fix] proyectos

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -3,7 +3,7 @@ import Cards from "../components/Cards"
 const Projects = () => {
   return (
     <div className="container-projects p-4">
-      <h1 className="f-bold text-white text-center">Proyectos:</h1>
+      <h1 className="f-bold text-white text-center text-uppercase">Proyectos :</h1>
       <div className="d-flex flex-wrap justify-content-around p-4">
         <Cards />
       </div>
