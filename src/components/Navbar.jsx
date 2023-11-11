ab0c8b355a63f06b8ed69cdcd03305681eca7d3d
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-11-11 13:02:50 -0300

    [fix] audio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,57 @@
+import { useState, useEffect } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 
 const Browser = () => {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+
   const setActiveClass = ({ isActive }) => {
-    return isActive ? "text-warning text-decoration-none me-3" : "text-secondary text-decoration-none me-3";
-  };
+    return isActive
+      ? "text-warning text-decoration-none me-3"
+      : "text-secondary text-decoration-none me-3"
+  }
+
+  const toggleAudio = () => {
+    setIsAudioPlaying(!isAudioPlaying)
+  }
+
+  useEffect(() => {
+    const audioElement = document.getElementById("audioPlayer")
+
+    if (isAudioPlaying) {
+      audioElement.play()
+    } else {
+      audioElement.pause()
+      audioElement.currentTime = 0
+    }
+  }, [isAudioPlaying])
 
   return (
-    <Navbar className='container-navbar' expand="md">
+    <Navbar className="container-navbar" expand="md">
       <Container>
         <Navbar.Brand>
           <div className="navbar-icon text-white text-decoration-none">
-            <i className="fa-solid fa-user-astronaut fa-spin fa-2xl" />
+            <button onClick={toggleAudio} className='button-icon-astro'>
+              <i className={`fa-solid fa-user-astronaut fa-spin fa-2xl ${isAudioPlaying ? 'playing' : 'paused'}`} />
+            </button>
           </div>
+          <audio id="audioPlayer" src="src/assets/Interstellar EPIC PIANO SUITE 12 min cover.mp3"></audio>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className='burguer-button' />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="burguer-button" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink to="/" className={setActiveClass}>Home</NavLink>
-            <NavLink to="/viewsdescription" className={setActiveClass}>Description</NavLink>
-            <NavLink to="/projects" className={setActiveClass}>Projects</NavLink>
-            <NavLink to="/Contacts" className={setActiveClass}>Contacts</NavLink>
+            <NavLink to="/" className={setActiveClass}>
+              Home
+            </NavLink>
+            <NavLink to="/viewsdescription" className={setActiveClass}>
+              Description
+            </NavLink>
+            <NavLink to="/projects" className={setActiveClass}>
+              Projects
+            </NavLink>
+            <NavLink to="/Contacts" className={setActiveClass}>
+              Contacts
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
