c114b649a76056118cedeb112f7860ab72a56c5d
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-11-11 14:01:34 -0300

    [fix] audio-ruta2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import interstellar from "../assets/interstellarCover.mp3"
 
 const Browser = () => {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false)
@@ -35,7 +36,7 @@ const Browser = () => {
               <i className={`fa-solid fa-user-astronaut fa-spin fa-2xl ${isAudioPlaying ? 'playing' : 'paused'}`} />
             </button>
           </div>
-          <audio id="audioPlayer" src="/src/assets/interstellarCover.mp3"></audio>
+          <audio id="audioPlayer" src={ interstellar }></audio>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className="burguer-button" />
         <Navbar.Collapse id="responsive-navbar-nav">
