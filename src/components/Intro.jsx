db9062b98a60c4d72b6e2d74449398fd620b9779
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-12-22 12:19:49 -0300

    [fix] /src

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,7 +13,7 @@ const Intro = () => {
         </div>
       </div>
       <div className="flex-grow-1 d-flex justify-content-end align-items-center me-4 animated-text-dos">
-        <img className="rounded-image" src="./src/assets/img/foto2.jpg" alt="foto2" />
+        <img className="rounded-image" src="https://raw.githubusercontent.com/hector1489/kaimanProject/main/src/assets/img/foto2.jpg" alt="foto2" />
       </div>
     </div>
   )
