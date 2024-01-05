8b9c7a859ce08f933846af0ffa988700ed077cef
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-01-05 11:48:09 -0300

    [fix] Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,31 +1,55 @@
+import { Button } from 'react-bootstrap'
 import Intro from "../components/Intro"
 import Description from "../components/Description"
-import Projects from "./Projects"
 import Form from "../components/Form"
 import Videos from "../components/Videos"
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-    return (
-        <>
-            <br />
-            <Intro id="top" />
-            <br />
-            <Description />
-            <br />
-            <div className="container-projects-dos">
-                <Videos />
-            </div>
-            <Projects />
-            <br />
-            <Form />
-            <br />
-            <div className="container-arrow d-flex">
-                <a href="#top" className="social-icon">
-                    <i className="fa-solid fa-angle-up fa-flip fa-2xl"></i>
-                </a>
-            </ div>
-        </>
-    );
+  const navigate = useNavigate()
+
+  const handleGoToprojects = () => {
+    navigate('/Projects')
+  }
+
+
+  return (
+    <>
+      <div className="mt-5">
+        <Intro />
+      </div>
+      <div className="mt-2">
+        <Description />
+      </div>
+      <div className="mt-2">
+        <Videos />
+      </div>
+      <div className="mt-5">
+        <div className='container-project-home'>
+          <h5
+          className='text-white fw-bold'
+          >
+          Puedes echar un vistazo a mis proyectos en <span className="text-warning"> :</span>
+          </h5>
+          <Button
+            className='css-button-gradient--5 fw-bold mb-2'
+            onClick={handleGoToprojects}
+          >
+            Ir a Proyectos
+          </Button>
+          </div>
+      </div>
+
+      <div className="mt-4">
+        <Form />
+      </div>
+      <div className="container-arrow d-flex">
+        <a href="#top" className="social-icon">
+          <i className="fa-solid fa-angle-up fa-flip fa-2xl"></i>
+        </a>
+      </ div>
+    </>
+  );
 }
 
 export default Home
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -3,9 +3,9 @@ import Cards from "../components/Cards"
 const Projects = () => {
   return (
     <div className="container-projects p-4">
-      <h1 className="f-bold text-white text-center text-uppercase animated-text-dos">
+      <h2 className="f-bold text-white text-center text-uppercase animated-text-dos">
         Proyectos
-      </h1>
+      </h2>
       <div className="d-flex flex-wrap justify-content-around p-4 animated-text">
         <Cards />
       </div>
