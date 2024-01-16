1a7914598c0e92a3fc1d293b32b4171264de1117
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-01-16 15:02:22 -0300

    [fix] button-video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -22,26 +22,32 @@ const Videos = () => {
   return (
     <div className='container-videos m-2'>
       <div className='d-flex flex-column-reverse flex-md-row'>
-      <div className='container-video col-md-6'>
-        <video ref={videoRef} width="100%" height="100%" controls autoPlay muted>
-          <source src={ video } type="video/mp4" />
-        </video>
+        <div className='container-video col-md-6'>
+          <video ref={videoRef} width="100%" height="100%" controls autoPlay muted>
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
+        <div className=' col-md-6'>
+          <div className='container-parrafo-video d-flex flex-column justify-content-center mt-2'>
+          <p className='text-center text-warning fw-bold p-2 animated-text-tres'>
+            Mi dedicación al perfeccionamiento de mis habilidades informáticas,
+            me ha permitido desarrollar la capacidad de crear un solido e-commerce para mi proyecto final en Desafios Latam.
+            En este puse a prueba mi adaptabilidad cambiando de javaSript a typeScript en las ultimas dos semanas.
+            Tiene como objetivo principal guardar la informacion de los usuarios de forma segura y obtener los datos de sus
+            productos favoritos del catologo puedes visitar la web y revisar la documentación en los siguientes enlaces :
+            <br />
+            <div className='button-links-video d-flex flex-column justify-content-center align-items-center mt-2'>
+              <a className='btn css-button-gradient--5 text-color-uno' href='https://frontend-svc7.onrender.com/' target='_blank' rel='noopener noreferrer'>
+                Web
+              </a>
+              <a className='btn css-button-gradient--5 text-color-uno' href='https://github.com/hector1489/IT-Dojo' target='_blank' rel='noopener noreferrer'>
+                Documentación
+              </a>
+            </div>
+          </p>
+          </div>
+        </div>
       </div>
-      <div className='container-parrafo-video col-md-6'>
-        <p className='text-center text-warning fw-bold p-2 animated-text-tres'>
-          Mi dedicación al perfeccionamiento de mis habilidades informáticas,
-          me ha permitido desarrollar la capacidad de crear un solido e-commerce para mi proyecto final en Desafios Latam.
-          En este puse a prueba mi adaptabilidad cambiando de javaSript a typeScript en las ultimas dos semanas.
-          Tiene como objetivo principal guardar la informacion de los usuarios de forma segura y obtener los datos de sus
-          productos favoritos del catologo puedes visitar la web y revisar la documentacion en los siguientes enlaces :
-          <br />
-          <Link className="text-color-uno"  to="https://frontend-svc7.onrender.com/" target="_blank">Web</Link>.
-          <br />
-          <Link className="text-color-uno"  to="https://github.com/hector1489/IT-Dojo" target="_blank">Documentacion</Link>.
-        </p>
-
-      </div>
-    </div>
 
     </div>
   )
