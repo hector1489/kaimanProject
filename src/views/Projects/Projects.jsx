3051cf7b92c9ad38a335529a80b66c864b7ff8b4
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-03 23:20:36 -0300

    [ fix ] css project

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -3,11 +3,11 @@ import './Projects.css'
 
 const Projects = () => {
   return (
-    <div className="container-projects p-4 animated-text-dos">
+    <div className="container-projects p-4">
       <h2 className="f-bold text-white text-center text-uppercase">
         Proyectos
       </h2>
-      <div className="d-flex flex-wrap justify-content-around p-4">
+      <div className="d-flex flex-wrap justify-content-around p-4 animated-text-dos">
         <Cards />
       </div>
     </div>
