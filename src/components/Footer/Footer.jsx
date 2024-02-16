cb4d92cc68ae34133bcaf434deae68b5826e009e
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-16 18:06:34 -0300

    [fix] footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className="container-footer text-center text-light p-2">
-            <h5>Gracias por visitar mi pagina que tengas buena exploracion ! 🚀 .@Hector_Gonzalez, 2023. </h5>
+            <p>Gracias por visitar mi pagina que tengas buena exploracion <span className='text-warning fw-bold' >!</span> 🚀 . <a href="https://github.com/hector1489"  target='_blank'>@Hector_Gonzalez</a> , 2024. </p>
         </footer>
     );
 }
