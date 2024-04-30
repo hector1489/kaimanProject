6a0688e7280f346a45cec7562766fcd0f286b861
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-04-29 21:24:44 -0400

    [fix] updateCV

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,4 +1,4 @@
-import cv from "../../assets/CV/HéctorGonzález2024.pdf"
+import cv from "../../assets/CV/HectorGonzalez04.pdf"
 import './Description.css'
 import { Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
