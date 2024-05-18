38731e3c8d01039ecc6ff29e4f32bd111f47d678
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-18 18:12:50 -0400

    [fix] border-img

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,11 +3,11 @@ import './Intro.css'
 const Intro = () => {
   return (
     <div className="container-intro">
-      <div className='d-flex flex-column justify-content-center gap-4'>
+      <div className="d-flex flex-column justify-content-center gap-4">
         <div className="container-intro-text text-center">
           <div className="p-2">
             <h1 className="fw-bold text-center">
-              "Desarrollador web <span className="text-info"> fullStack </span><span className="text-warning">.</span> "
+              "Desarrollador web <span className="text-info">fullStack</span><span className="text-warning">.</span>"
             </h1>
           </div>
 
@@ -18,11 +18,11 @@ const Intro = () => {
 
         <div className="d-flex flex-column flex-md-row mt-3">
           <div className="container-project-home text-center p-2">
-            <p className='text-white'>
-              <span className='text-warning fw-bold'>¡Hola!</span> Soy un apasionado <span className='text-info fw-bold'>desarrollador web Full Stack.</span> Con
+            <p className="text-white">
+              <span className="text-warning fw-bold">¡Hola!</span> Soy un apasionado <span className="text-info fw-bold">desarrollador web Full Stack.</span> Con
               experiencia en trabajos freelance para la Agrupación Amigos del Puangue donde me desenvuelvo
               como director y desarrollador web. Mis años previos están centrados en la salud con 14 años de carrera en
-              en transporte, rescates y urgencias.
+              transporte, rescates y urgencias.
             </p>
           </div>
         </div>
