82e85cb4addc8008468b53604acb4290a4f7fb9c
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-25 18:10:47 -0400

    [fix] del padding

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer className="container-footer text-center text-light p-2">
+        <footer className="text-center text-light p-2">
             <p>Gracias por visitar mi pagina que tengas buena exploracion <span className='text-warning fw-bold' >!</span> 🚀 . <a href="https://github.com/hector1489"  target='_blank'>@Hector_Gonzalez</a> , 2024. </p>
         </footer>
     );
