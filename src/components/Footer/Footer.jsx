5c80cd50e7e83a1141dd15252711f517cc6fa136
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-15 15:45:20 -0400

    [fix] clean css footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import './Footer.css'
 
 const Footer = () => {
-    return (
-        <footer className="d-flex justify-content-center align-items-center text-center text-light p-2">
-            <p>Gracias por visitar mi pagina que tengas buena exploracion <span className='text-warning fw-bold' >!</span> 🚀 . <a href="https://github.com/hector1489"  target='_blank'>@Hector_Gonzalez</a> , 2024. </p>
-        </footer>
-    );
+  return (
+    <footer className="d-flex justify-content-center align-items-center text-center text-light p-2">
+      <p>Gracias por visitar mi pagina que tengas buena exploracion <span className='text-warning fw-bold' >!</span> 🚀 . <a href="https://github.com/hector1489" target='_blank'>@Hector_Gonzalez</a> , 2024. </p>
+    </footer>
+  );
 }
 
 export default Footer
