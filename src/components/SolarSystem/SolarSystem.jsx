bdcb166c49a53d9d82c8f80a6ea7591005d7e8af
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-12-11 10:37:27 -0300

    fix clean

--- src/components/SolarSystem/SolarSystem.jsx
+++ src/components/SolarSystem/SolarSystem.jsx
@@ -206,8 +206,6 @@ const SolarSystem = () => {
     
       cometsRef.current.push({ comet, cometTail });
     };
-    
-    
 
     // Crear cometas aleatorios 
     const cometInterval = setInterval(createComet, 5000);
