ac8879463c512bd7b0a6aa12bbf56adfb321b51a
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-12-14 11:58:53 -0300

    fix cometas fuera de camara

--- src/components/SolarSystem/SolarSystem.jsx
+++ src/components/SolarSystem/SolarSystem.jsx
@@ -8,8 +8,7 @@ import planetaRojo from "../../assets/planetas/planetaRojo.png";
 import planetaAzul from "../../assets/planetas/planetaAzul.png";
 import planetaPurple from "../../assets/planetas/planetaPurple.png";
 import planetaVerde from "../../assets/planetas/planetaVerde.png";
-import smokeCometa from "../../assets/planetas/smokeBlue2.png"
-import moonTexture from "../../assets/planetas/textureMoon.png"
+import smokeCometa from "../../assets/planetas/smokeBlue2.png";
 
 const SolarSystem = () => {
   const mountRef = useRef(null);
@@ -19,6 +18,7 @@ const SolarSystem = () => {
   const navigate = useNavigate();
   const planetsRef = useRef([]);
   const cometsRef = useRef([]);
+  const cometGenerationActive = useRef(true);
 
   const sunRotationSpeedRef = useRef(sunRotationSpeed);
 
@@ -32,7 +32,6 @@ const SolarSystem = () => {
     renderer.setSize(width, height);
     mountRef.current.appendChild(renderer.domElement);
 
-    // Crear CSS2DRenderer para etiquetas
     const labelRenderer = new CSS2DRenderer();
     labelRenderer.setSize(width, height);
     labelRenderer.domElement.style.position = "absolute";
@@ -41,14 +40,12 @@ const SolarSystem = () => {
 
     scene.background = null;
 
-    // Luz direccional y ambiental
     const light = new THREE.DirectionalLight(0xffffff, 2);
     light.position.set(0, 1, 1).normalize();
     scene.add(light);
     const ambientLight = new THREE.AmbientLight(0x404040, 0.5);
     scene.add(ambientLight);
 
-    // Sol con material emissivo
     const sunGeometry = new THREE.SphereGeometry(2, 128, 128);
     const sunTextureMap = new THREE.TextureLoader().load(solTexture);
     const sunMaterial = new THREE.MeshStandardMaterial({
@@ -63,7 +60,6 @@ const SolarSystem = () => {
     const sun = new THREE.Mesh(sunGeometry, sunMaterial);
     scene.add(sun);
 
-    // Resplandor del Sol
     const glowRadius = [2.5, 3, 3.5, 4, 4.5];
     const glowOpacity = [0.2, 0.15, 0.1, 0.05, 0.02];
 
@@ -78,7 +74,6 @@ const SolarSystem = () => {
       scene.add(glow);
     });
 
-    // Cargar las texturas de los planetas
     const textures = {
       planetaRojo: new THREE.TextureLoader().load(planetaRojo),
       planetaAzul: new THREE.TextureLoader().load(planetaAzul),
@@ -100,7 +95,6 @@ const SolarSystem = () => {
       planet.position.x = distance;
       planet.userData = { link };
 
-      // Crear una etiqueta con el nombre del planeta
       const label = document.createElement("div");
       label.className = "planet-label";
       label.textContent = name;
@@ -114,8 +108,9 @@ const SolarSystem = () => {
       return { planet, distance, angle: Math.random() * Math.PI * 2 };
     });
 
-    // Función para crear cometas
     const createComet = () => {
+      if (!cometGenerationActive.current) return;
+
       const cometGeometry = new THREE.SphereGeometry(0.15, 8, 8);
       const cometMaterial = new THREE.MeshStandardMaterial({
         color: 0xffffff,
@@ -126,21 +121,18 @@ const SolarSystem = () => {
         transparent: true,
         opacity: 0.8,
       });
-    
+
       const comet = new THREE.Mesh(cometGeometry, cometMaterial);
-    
-      // Posición inicial aleatoria
+
       comet.position.set(
         Math.random() * 20 - 10,
         Math.random() * 10 - 5,
         Math.random() * 20 - 10
       );
-    
-      // Parámetros de la cola del cometa (usando partículas)
+
       const tailLength = 50;
       const tailParticles = [];
-    
-      // Material de las partículas de la cola
+
       const cometTailMaterial = new THREE.PointsMaterial({
         size: 0.1,
         sizeAttenuation: true,
@@ -150,69 +142,69 @@ const SolarSystem = () => {
         map: new THREE.TextureLoader().load(smokeCometa),
         blending: THREE.AdditiveBlending,
       });
-    
+
       // Crear partículas de la cola
       for (let i = 0; i < tailLength; i++) {
         const particle = new THREE.Vector3(comet.position.x, comet.position.y, comet.position.z);
         tailParticles.push(particle);
       }
-    
+
       const cometTailGeometry = new THREE.BufferGeometry().setFromPoints(tailParticles);
       const cometTail = new THREE.Points(cometTailGeometry, cometTailMaterial);
-    
+
       scene.add(comet);
       scene.add(cometTail);
-    
+
       // Movimiento del cometa y la cola
       let direction = new THREE.Vector3(
         Math.random() * 0.1 - 0.05,
         Math.random() * 0.1 - 0.05,
         Math.random() * 0.1 - 0.05
       );
-    
+
       const animateComet = () => {
         comet.position.add(direction);
-    
+
         // Actualizar las partículas de la cola
         for (let i = tailLength - 1; i > 0; i--) {
           tailParticles[i].copy(tailParticles[i - 1]);
         }
         tailParticles[0] = new THREE.Vector3(comet.position.x, comet.position.y, comet.position.z);
-    
+
         cometTailGeometry.attributes.position.needsUpdate = true;
-    
+
         // Atenuar el cometa y la cola con el tiempo
         cometMaterial.opacity -= 0.001;
         cometTailMaterial.opacity -= 0.001;
-    
-        // Agregar un brillo a la cola con un gradiente de opacidad (más cerca del cometa es más brillante)
-        for (let i = 0; i < tailLength; i++) {
-          const particle = tailParticles[i];
-          const distanceFromComet = particle.distanceTo(comet.position);
-          const tailOpacity = Math.max(0, 1 - distanceFromComet * 0.02);
-          cometTailMaterial.opacity = tailOpacity;
-        }
-    
+
         // Eliminar el cometa y la cola cuando están demasiado lejos o atenuados
         if (comet.position.length() > 30 || cometMaterial.opacity <= 0.1) {
           scene.remove(comet);
           scene.remove(cometTail);
         }
-    
+
         requestAnimationFrame(animateComet);
       };
-    
+
       animateComet();
-    
+
       cometsRef.current.push({ comet, cometTail });
     };
 
-    // Crear cometas aleatorios 
     const cometInterval = setInterval(createComet, 5000);
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        cometGenerationActive.current = false;
+      } else {
+        cometGenerationActive.current = true;
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
 
     camera.position.z = cameraZ;
 
-    // Interpolación suave de la cámara y del Sol
     let targetCameraZ = cameraZ;
     let targetCameraRotation = { ...cameraRotation };
     let targetSunRotationSpeed = sunRotationSpeed;
@@ -220,10 +212,8 @@ const SolarSystem = () => {
     const animate = () => {
       requestAnimationFrame(animate);
 
-      // Rotación suave del Sol
       sun.rotation.y += sunRotationSpeedRef.current;
 
-      // Movimiento orbital suave de los planetas
       planetsRef.current.forEach(({ planet, distance, angle }, index) => {
         angle += 0.0005;
         planet.position.x = Math.cos(angle) * distance;
@@ -231,12 +221,10 @@ const SolarSystem = () => {
         planetsRef.current[index].angle = angle;
       });
 
-      // Suavizado de la posición de la cámara con Lerp
       camera.position.z = THREE.MathUtils.lerp(camera.position.z, targetCameraZ, 0.05);
       camera.rotation.x = THREE.MathUtils.lerp(camera.rotation.x, targetCameraRotation.x, 0.05);
       camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, targetCameraRotation.y, 0.05);
 
-      // Renderizado
       renderer.render(scene, camera);
       labelRenderer.render(scene, camera);
     };
@@ -246,21 +234,17 @@ const SolarSystem = () => {
     const handleScroll = (event) => {
       const delta = event.deltaY;
 
-      // Suavizado de la velocidad de rotación del Sol
       targetSunRotationSpeed = sunRotationSpeed + delta * 0.0000005;
       sunRotationSpeedRef.current = THREE.MathUtils.lerp(sunRotationSpeedRef.current, targetSunRotationSpeed, 0.1);
 
-      // Suavizado de la distancia de la cámara
       targetCameraZ = Math.max(10, targetCameraZ - delta * 0.02);
 
-      // Suavizado de la rotación de la cámara
       targetCameraRotation = {
         x: cameraRotation.x + delta * 0.0005,
         y: cameraRotation.y + delta * 0.0005,
       };
     };
 
-    // Manejo de clics sobre los planetas
     const onMouseClick = (event) => {
       const mouse = new THREE.Vector2();
       mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
@@ -278,12 +262,11 @@ const SolarSystem = () => {
       }
     };
 
-    // Agregar eventos
     window.addEventListener("wheel", handleScroll);
     window.addEventListener("click", onMouseClick);
 
-    // Limpieza de eventos
     return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
       window.removeEventListener("wheel", handleScroll);
       window.removeEventListener("click", onMouseClick);
       clearInterval(cometInterval);
@@ -298,4 +281,3 @@ const SolarSystem = () => {
 };
 
 export default SolarSystem;
- 
