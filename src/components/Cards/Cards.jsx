cbb255eaaa14a44b005286ca952fefc77122e9eb
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2025-03-01 19:30:16 -0300

    fix card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -10,22 +10,22 @@ const Cards = () => {
   const sliceData = data.slice(0, 14)
 
   return (
-      <div className="cards-container">
-        {sliceData.map((item, index) => (
-          <Card key={item.image} className="card">
-            <Card.Img variant="top" src={item.image} alt={item.name} />
-            <Card.Body>
-              <div className="container-card">
-                <Card.Title className="card-title">{item.name}</Card.Title>
-                <Card.Text className="card-text text-warning">{item.description}</Card.Text>
-                <Link to={item.url} target="_blank">
-                  <Button className="btn css-button-gradient--5 fw-bold">GitHub</Button>
-                </Link>
-              </div>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
+    <div className="cards-container">
+      {sliceData.map((item) => (
+        <Card key={item.image} className="card">
+          <Card.Img variant="top" src={item.image} alt={item.name} />
+          <Card.Body>
+            <div className="container-card">
+              <Card.Title className="card-title">{item.name}</Card.Title>
+              <Card.Text className="card-text text-warning">{item.description}</Card.Text>
+              <Link to={item.url} target="_blank">
+                <Button className="btn css-button-gradient--5 fw-bold">GitHub</Button>
+              </Link>
+            </div>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   )
 }
 
